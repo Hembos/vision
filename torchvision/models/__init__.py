cfebd0b6f3ad6deee2bfd526b0f5e899960f15ec
Hembos <faizrahmanov.artur@mail.ru>
2023-02-27 12:46:42 +0300

    Generalized rcnn model return detectrions and loss

--- torchvision/models/__init__.py
+++ torchvision/models/__init__.py
@@ -13,7 +13,7 @@ from .squeezenet import *
 from .vgg import *
 from .vision_transformer import *
 from .swin_transformer import *
-from .maxvit import *
+# from .maxvit import *
 from . import detection, optical_flow, quantization, segmentation, video
 
 # The Weights and WeightsEnum are developer-facing utils that we make public for
